7645b0cdc8d07337d64166d4544de9c019629ea9
Snekkja <sneekja@snekkja.fr>
2016-07-17 12:29:09 +0200

    Jour 2 - matin
    Event AJAX, Formulaire avancée et structure (if / check)

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,42 +1,43 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 export class Bouteille {
-    reference: string;
-    designation: string;
-    contenance: number;
-    annee: number;
+    reference:string;
+    designation:string;
+    contenance:number;
+    annee:number;
 }
 
 @Component({
     selector: 'wineo',
 
-    template:
-        `
+    template: `
             <h1>Stockage de bouteille en ligne</h1>
             
-            <div>
-                <h2>Fiche de la bouteille</h2>
-                <div>{{ bouteille.reference }}</div>
-                <div>{{ bouteille.designation }}</div>
-                <div>{{ bouteille.annee }}</div>
-                <div>{{ bouteille.contenance }}</div>
-            </div>
-            
-            <div>
-                <h2>Formulaire de modification</h2>
-                <div>
-                    <label>Référence :</label>
-                    <input [(ngModel)]="bouteille.designation" placeholder="designation"/>
-                </div>
+            <div id="listeBouteilles">
+                <h2>Fiches des bouteilles</h2>
                 
+                <ul>
+                    <li *ngFor="let bouteille of cave" (click)="onSelect(bouteille)">
+                        <div>{{ bouteille.reference }}</div>
+                    </li>
+                </ul>
+            </div>
+         
+            <div id="detailBouteille" *ngIf="bouteilleSelectionnee">
+                <h2>Détail de la bouteille sélectionnée</h2>
+                
+                <div>Référence : {{ bouteilleSelectionnee.reference }}</div>
+                <div>
+                    <label>Désignation :</label>
+                    <input [(ngModel)]="bouteilleSelectionnee.designation" />
+                </div>
                 <div>
                     <label>Année :</label>
-                    <input [(ngModel)]="bouteille.annee" placeholder="2000"/>
+                    <input [(ngModel)]="bouteilleSelectionnee.annee" />
                 </div>
-                
                 <div>
                     <label>Contenance :</label>
-                    <input [(ngModel)]="bouteille.contenance" placeholder="0.75"/>
+                    <input [(ngModel)]="bouteilleSelectionnee.contenance" />
                 </div>
             </div>
         `
@@ -44,11 +45,39 @@ export class Bouteille {
 
 export class AppComponent {
 
-    bouteille: Bouteille = {
-        reference: 'XFRDF',
-        designation: 'Lambrusco',
-        contenance: 0.75,
-        annee: 2016
-    };
+    cave = CAVE;
+
+    bouteilleSelectionnee: Bouteille;
+
+    onSelect(bouteille: Bouteille) {
+        this.bouteilleSelectionnee= bouteille;
+    }
 
 }
+
+const CAVE: Bouteille[] = [
+    {
+        reference: 'XFRDFOPZD',
+        designation: 'Lambrusco',
+        contenance: 0.75,
+        annee: 2014
+    },
+    {
+        reference: 'DUHGJWJE',
+        designation: 'Sauvignon',
+        contenance: 0.75,
+        annee: 2011
+    },
+    {
+        reference: 'PWETPWET',
+        designation: 'Champagne',
+        contenance: 1,
+        annee: 2012
+    },
+    {
+        reference: 'JAVACKK',
+        designation: 'Chablis',
+        contenance: 0.75,
+        annee: 2011
+    }
+];
