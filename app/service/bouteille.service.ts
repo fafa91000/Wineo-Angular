199d17b723f18c41e32d8cafdeb524a8b7921f3f
Snekkja <sneekja@snekkja.fr>
2016-07-17 15:45:26 +0200

    Jour 5 après-midi
    Consommation du WS REST de Wineo-Middleware

--- app/service/bouteille.service.ts
+++ app/service/bouteille.service.ts
@@ -1,48 +1,42 @@
+import {Http, Headers} from "@angular/http";
 import {Injectable} from "@angular/core";
-import {Bouteille} from '../domain/bouteille';
+import {Bouteille} from "../domain/bouteille";
 
 @Injectable()
 export class BouteilleService {
 
-    // Obtenir une promesse sur les bouteilles de la cave
-    obtenirBouteilles() {
-        return Promise.resolve(CAVE);
+    // URL du WS d'obtention des bouteilles
+    private urlWSBouteille: String = 'http://localhost:8080/tutu';
+
+    constructor(private http: Http) { }
+
+    /*
+     *   Obtenir une promesse sur les bouteilles de la cave
+     */
+    obtenirBouteilles(): Promise<Bouteille[]> {
+
+        // Nous consommons le WS
+        return this.http
+            .get(this.urlWSBouteille)
+            .toPromise()
+            .then(reponse => reponse.json().data);
     }
 
-    // Obtenir une promesse sur les bouteilles de la cave avec de la latence
-    obtenirBouteillesAvecDeLaLatence(tempsLatence: number) {
-        return new Promise<Bouteille[]>(resolve => setTimeout(() => resolve(CAVE), tempsLatence));
-    }
-
+    /*
+     * Ajouter une bouteille.
+     */
     ajouterBouteille(bouteille: Bouteille) {
-        return new Promise<number>(resolve => CAVE.push(bouteille));
+
+        // Nous préparons les paramètres d'entêtes
+        let headers = new Headers({
+            'Content-Type': 'application/json'
+        });
+
+        // Nous consommons le WS
+        return this.http
+            .post(this.urlWSBouteille, JSON.stringify(bouteille), {headers: headers})
+            .toPromise()
+            .then(reponse => reponse.json().data);
     }
 
 }
-
-var CAVE: Bouteille[] = [
-    {
-        reference: 'XFRDFOPZD',
-        designation: 'Lambrusco',
-        contenance: 0.75,
-        annee: 2014
-    },
-    {
-        reference: 'DUHGJWJE',
-        designation: 'Sauvignon',
-        contenance: 0.75,
-        annee: 2011
-    },
-    {
-        reference: 'PWETPWET',
-        designation: 'Champagne',
-        contenance: 1,
-        annee: 2012
-    },
-    {
-        reference: 'JAVACKK',
-        designation: 'Chablis',
-        contenance: 0.75,
-        annee: 2011
-    }
-];
